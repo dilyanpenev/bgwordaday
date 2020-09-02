a2a0ab4d592652c2cee7561ac17666884508df28
g72446dp <dilyan.penev@student.manchester.ac.uk>
2020-09-02 21:26:03 +0300

    sharing by email

--- words/static/words/js/sidebar.js
+++ words/static/words/js/sidebar.js
@@ -1,3 +1,19 @@
 function toggleSidebar(){
     document.getElementById("sidebar").classList.toggle('active');
 }
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const btn = document.getElementsByClassName("label")[0];
+    const email = document.getElementsByClassName("email")[0];
+
+    btn.disabled = true;
+
+    email.onkeyup = () => {
+        if (email.value.length > 0) {
+            btn.disabled = false;
+        } else {
+            btn.disabled = true;
+        }
+    }
+});
--- words/views.py
+++ words/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django import forms
 from .models import Word
 import datetime
 
 # Create your views here.
 def todays_word(request):
+
+    today = datetime.date.today()
+    word = Word.objects.get(date=today)
+    subject = 'Your daily Bulgarian word!'
+    message = f'The word for {today} is {word.bgword}.'
+
+    if request.method == 'POST':
+        send_mail(subject, message, 'baldrianmockingjay@gmail.com', [request.POST.get('email')])
+
     return render(request,
                     'words/pages/index.html',
-                    {'word': Word.objects.get(date=datetime.date.today())})
+                    {'word': word})
